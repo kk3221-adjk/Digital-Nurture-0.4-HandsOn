071bf5bcf8da5e3e949456a230f41f9edac7a09e
kk3221-adjk <adrija28104@gmail.com>
2025-06-22 13:50:51 +0530

    Output image added

---
+++ Week1/Design Principles and Patterns/Implementing Singleton Pattern/SingletonPattern/Logger.cs
@@ -0,0 +1,10 @@
+using System;
+using System.collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week1.DesignPrinciplesandPatterns.ImplementatingSingletonPattern.SingletonPattern
+{
+    
+}
