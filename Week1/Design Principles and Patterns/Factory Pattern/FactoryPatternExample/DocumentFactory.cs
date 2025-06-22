3d2c3bf726f1cf80911cd873031446b4faf4db3a
kk3221-adjk <adrija28104@gmail.com>
2025-06-22 20:34:11 +0530

    Factory Update

---
+++ Week1/Design Principles and Patterns/Factory Pattern/FactoryPatternExample/DocumentFactory.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethodPatternExample
+{
+    public abstract class DocumentFactory
+    {
+        public abstract Document createDocument();
+    }
+}
--- Week1/Design Principles and Patterns/Factory Pattern/FactoryPatternExample/Program.cs
+++ Week1/Design Principles and Patterns/Factory Pattern/FactoryPatternExample/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethodPatternExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DocumentFactory wordFactory = new WordDocumentFactory();
+            Document wordDoc = wordFactory.createDocument();
+            wordDoc.open();
+
+            DocumentFactory pdfFactory = new PDFDocumentFactory();
+            Document pdfDoc = pdfFactory.createDocument();
+            pdfDoc.open();
+
+            DocumentFactory excelFactory = new ExcelDocumentFactory();
+            Document excelDoc = excelFactory.createDocument();
+            excelDoc.open();
+
+            Console.ReadLine();
+
+        }
+    }
+}
